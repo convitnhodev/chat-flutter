40dd74253febb80fe0005ecda43ae61ce4b17632
Hùng Thái Nguyễn Việt <hungthainguyenviet@192.168.1.4>
2023-10-01 01:34:15 +0700

    chore: get key regarding profile

--- lib/common/store/user.dart
+++ lib/common/store/user.dart
@@ -1,15 +1,32 @@
+import 'dart:convert';
+import 'dart:html';
+
+import 'package:chatty/common/entities/user.dart';
+import 'package:chatty/common/store/storage.dart';
+import 'package:chatty/common/values/storage.dart';
 import 'package:get/get.dart';
-
-
 
 class UserStore extends GetxController {
   static UserStore get to => Get.find();
-
 
   final _isLogin = false.obs;
 
   String token = '';
 
+  final _profile = UserItem().obs;
 
   bool get isLogin => _isLogin.value;
+  UserItem get profile => _profile.value;
+  bool get hasToken => token.isNotEmpty;
+
+  @override
+  void onInit() {
+    super.onInit();
+    token = StorageService.to.getString(STORAGE_USER_TOKEN_KEY);
+    var profileOffline = StorageService.to.getString(STORAGE_USER_PROFILE_KEY);
+    if (profileOffline.isNotEmpty) {
+      _isLogin.value = true;
+      _profile(UserItem.fromJson(jsonDecode(profileOffline)));
+    }
+  }
 }
